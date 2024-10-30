2c410e7eb0c5dce59eca7945f6e399be8c722b72
forworldm <7335354+forworldm@users.noreply.github.com>
2024-10-30 18:16:20 +0800

    126

--- android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "125.0.20240408174943"
+    const val version = "126.0.20240526221752"
 
     /**
      * GeckoView channel
